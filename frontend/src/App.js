94cd5a10ef2cb4185dec21b53518a63df9488485
goosefring <bhattirehan.rb@gmail.com>
2023-09-01 10:39:13 -0600

    connected to API, populating data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,6 @@ function App() {
         <Route path="/" Component={MainPage}>
         </Route>
       </Routes>
-      <Coins />
       </div>
     </Router>
   );
--- frontend/src/components/Coins.jsx
+++ frontend/src/components/Coins.jsx
@@ -1,28 +1,42 @@
 import axios from 'axios'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Coins = () => {
+    const [coins, setCoins] = useState([]);
 
-    let [[coins], setCoins] = useState([]);
+    useEffect(() => {
+        const fetchCoins = async () => {
+            try {
+                const res = await fetch('https://api.coingecko.com/api/v3/search/trending');
+                const data = await res.json();
+                //console.log('coin data: ' + JSON.stringify(data));
+                setCoins(data);
+            } catch (err) {
+                console.error('Data Error: ' + err)
+            }
+        }
+        fetchCoins();
+    }, []);
 
-    try {
-        const res = axios.get('https://api.coingecko.com/api/v3/search/trending');
-        console.log('coin data: ' + res.data);
-        setCoins = res;
-        console.log('coin array: ' + coins);
-    } catch (err) {
-        console.error('Data Error: ' + err)
-    }
+    //console.log(coins);
 
-    // return (
-    //     <div className="coins">
-    //         <p>Trending Coins</p>
-    //         {/* <ul>
-    //             {coins.map(coin => {
-    //                 <li>{coin}</li>  
-    //             })}
-    //         </ul> */}
-    //     </div>
-    // )
+    coins.map(coin => {
+        console.log(coin.item.name);
+    })
+
+    return (
+        <div className="coins">
+            <p>Trending Coins</p>
+            <ul>
+            {coins.length > 0 ? (
+                coins.map((coin, i) =>
+                <li key={coin[++i].item.coin_id}>{coin[++i].item.name}</li>  
+                )
+                ) : (
+                    <li>No coins available</li>
+                )}
+            </ul>
+        </div>
+    )
 }
 export default Coins;
