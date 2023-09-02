1d250b3d0d69412d6c321efda2480e76dd6b2433
goosefring <bhattirehan.rb@gmail.com>
2023-09-02 17:32:39 -0600

    added basic search

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -19,19 +19,23 @@ const MainPage = () => {
         try {
             const res = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`);
             const data = await res.json();
+            console.log('data', data);
             const coinArray = data.coins.map(coin => {
                 return {
-                    name: coin.item.name,
-                    symbol: coin.item.symbol,
-                    image: coin.item.large,
+                    name: coin.name,
+                    symbol: coin.symbol,
+                    image: coin.large,
                 }
             });
             setSearchedCoins(coinArray);
-            console.log('searched coins: ' + searchedCoins);
+            console.log('coin array in function: ' + coinArray);
         } catch (err) {
             console.error('Data Error: ' + err)
         }
     }
+
+    console.log('query: ', query);
+    console.log('searched coins: ', searchedCoins);
 
     return (
         <div className="main">
