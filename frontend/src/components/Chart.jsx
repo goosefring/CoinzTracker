1dd3ff331e1dd801604b06ca12ac1da4c1a47a7f
goosefring <bhattirehan.rb@gmail.com>
2023-09-08 17:15:29 -0600

    minor changes

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -6,51 +6,55 @@ const Chart = (coinGraph) => {
     console.log('chart props', coinGraph);
 
 
-    // class ApexChart extends React.Component {
+    class ApexChart extends React.Component {
 
-    //     constructor(coinGraph) {
-    //         super(coinGraph);
+        constructor(coinGraph) {
+            super(coinGraph);
+
+            for (let i = 0; i < coinGraph.length; i++) {
+
+                this.state = {
     
-    //         this.state = {
+                    series: [{
+                        data: [{
+                            x: coinGraph[i].Date,
+                            y: [coinGraph[i].Open, coinGraph[i].High, coinGraph[i].Low, coinGraph[i].Close] 
+                        },
+                        ]
+                    }],
+                    options: {
+                        chart: {
+                            type: 'candlestick',
+                            height: 350
+                        },
+                        title: {
+                            text: 'CandleStick Chart',
+                            align: 'left'
+                        },
+                        xaxis: {
+                            type: 'datetime'
+                        },
+                        yaxis: {
+                            tooltip: {
+                                enabled: true
+                            }
+                        }
+                    },
+                };
+            }
+
+        }
     
-    //             series: [{
-    //                 data: [{
-    //                     x: coinGraph[0],
-    //                     y: coinGraph[1]
-    //                 },
-    //                 ]
-    //             }],
-    //             options: {
-    //                 chart: {
-    //                     type: 'candlestick',
-    //                     height: 350
-    //                 },
-    //                 title: {
-    //                     text: 'CandleStick Chart',
-    //                     align: 'left'
-    //                 },
-    //                 xaxis: {
-    //                     type: 'datetime'
-    //                 },
-    //                 yaxis: {
-    //                     tooltip: {
-    //                         enabled: true
-    //                     }
-    //                 }
-    //             },
-    //         };
-    //     }
-    
-    //     render() {
-    //         return (
-    //             <div id="chart">
-    //                 <ReactApexChart options={this.state.options} series={this.state.series} type="candlestick" height={350} />
-    //             </div>
-    //         );
-    //     }
-    // }
-    // const domContainer = document.querySelector('#app');
-    // ReactDOM.render(React.createElement(ApexChart), domContainer);
+        render() {
+            return (
+                <div id="chart">
+                    <ReactApexChart options={this.state.options} series={this.state.series} type="candlestick" height={350} />
+                </div>
+            );
+        }
+    }
+    const domContainer = document.querySelector('#app');
+    ReactDOM.render(React.createElement(ApexChart), domContainer);
 }
 
 export default Chart;
